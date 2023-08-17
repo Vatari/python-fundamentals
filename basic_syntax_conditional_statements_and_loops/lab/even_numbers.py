b84390f5a1daa35ae5deda3c96f0de65da893820
vatari <majestic13@gmail.com>
2023-08-17 09:25:19 +0300

    exercise

---
+++ basic_syntax_conditional_statements_and_loops/lab/even_numbers.py
@@ -0,0 +1,10 @@
+num = int(input())
+for i in range(num):
+    next_num = int(input())
+
+    if next_num % 2 != 0:
+        print(f"{next_num} is odd!")
+        break
+else:
+    print("All numbers are even.")
+    
