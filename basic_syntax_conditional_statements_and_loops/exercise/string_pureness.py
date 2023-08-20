c3210e63b63a87d20290fce608316f6bbc835685
vatari <majestic13@gmail.com>
2023-08-20 09:11:52 +0300

    exercise

---
+++ basic_syntax_conditional_statements_and_loops/exercise/string_pureness.py
@@ -0,0 +1,12 @@
+num_of_strings = int(input())
+
+for num in range(num_of_strings):
+    text = input()
+    is_pure_string = True
+    for i in text:
+        if i in (".", ",", "_"):
+            print(f'{text} is not pure!')
+            is_pure_string = False
+            break
+    if is_pure_string:
+        print(f'{text} is pure.')
